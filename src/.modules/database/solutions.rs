fbc8d6dca1ea3b93f71f5824769f65bbfa0df4c5
kaikiat <pohkaikiat98@gmail.com>
2023-06-01 09:24:26 +0800

    fix serde

--- src/.modules/database/solutions.rs
+++
@@ -1,4 +0,0 @@
-pub fn get_solutions(connection: &PgConnection) -> QueryResult<Vec<Solution>>  {
-    solutions.limit(5)
-        .load::<Solution>(&*connection)
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,74 @@
-mod database;
-mod models;
-mod routes;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use rocket::serde::json::{json, Value};
+
+#[macro_use]
+extern crate rocket_sync_db_pools;
+
+extern crate rocket_cors;
+use rocket_cors::{Cors, CorsOptions};
+
+#[macro_use]
+extern crate diesel;
+
+#[macro_use]
+extern crate validator_derive;
+
+use dotenv::dotenv;
 
 mod config;
+mod database;
 mod errors;
+mod models;
+mod routes;
+mod schema;
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+fn cors_fairing() -> Cors {
+    CorsOptions::default()
+        .to_cors()
+        .expect("Cors fairing cannot be created")
+}
+
+#[launch]
+pub fn rocket() -> _ {
+    dotenv().ok();
+    rocket::custom(config::from_env())
+        .mount(
+            "/api",
+            routes![
+                routes::users::post_users,
+                routes::users::post_users_login,
+                routes::users::put_user,
+                routes::users::get_user,
+                routes::articles::post_articles,
+                routes::articles::put_articles,
+                routes::articles::get_article,
+                routes::articles::delete_article,
+                routes::articles::favorite_article,
+                routes::articles::unfavorite_article,
+                routes::articles::get_articles,
+                routes::articles::get_articles_feed,
+                routes::articles::post_comment,
+                routes::articles::get_comments,
+                routes::articles::delete_comment,
+                routes::tags::get_tags,
+                routes::profiles::get_profile,
+                routes::profiles::follow,
+                routes::profiles::unfollow,
+            ],
+        )
+        .attach(database::Db::fairing())
+        .attach(cors_fairing())
+        .attach(config::AppState::manage())
+        .register("/", catchers![not_found])
+}
