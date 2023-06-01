1ef6b5fde26434795d95b89ff3522391c3d02264
kaikiat <pohkaikiat98@gmail.com>
2023-06-01 09:54:52 +0800

    update cargo.toml

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,8 @@
 use rocket::config::Config;
-use rocket::fairing::AdHoc;
 use rocket::figment::Figment;
 use std::collections::HashMap;
 use std::env;
 
-/// Debug only secret for JWT encoding & decoding.
-// const SECRET: &'static str = "8Xui8SN4mI+7egV/9dlfYYLGQJeEx4+DwmSQLwDVXJg=";
-
-/// js toISOString() in test suit can't handle chrono's default precision
 pub const DATE_FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%.3fZ";
 
 pub const TOKEN_PREFIX: &'static str = "Token ";
