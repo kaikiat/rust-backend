5491a49c158cd2494bf31fb42bed6c5a3772e872
kaikiat <pohkaikiat98@gmail.com>
2023-06-02 15:10:21 +0800

    update

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # rust-backend
+
+## Getting started
 1. Use diesel-cli for postgress setup [link](https://genekuo.medium.com/creating-a-rest-api-in-rust-with-persistence-rust-rocket-and-diesel-a4117d400104)
 2. `diesel setup --database-url=postgresql://localhost:5432/leetcode`
 3. `diesel migration generate create_solutions`
 4. `diesel migration run`
 5. `diesel print-schema > src/schema.rs` | `diesel print-schema > src/database/schema.rs`
 6. Connect to psql locally psql -h localhost -d $database_name -U $username or psql -h localhost -d leetcode
+
+## Pipeline
+1. docker build --platform linux/amd64 -t rust-backend .
+2. Tag if not tagged docker tag tm-test kaikiatpoh/rust-backend
+3. docker push kaikiatpoh/rust-backend
+4. sudo docker logs -f kaikiatpoh/rust-backend
+5. Further optimisation `DOCKER_BUILDKIT=0 docker buildx build --ulimit nofile=1024000:1024000 --platform linux/amd64 .`
+
 
 ## Others
 1. Refer to [link](https://github.com/TatriX/realworld-rust-rocket)
--- src/routes/solutions.rs
+++ src/routes/solutions.rs
@@ -1,4 +1,4 @@
-use crate::database::{self, Db};
+use crate::{database::{self, Db}};
 use rocket::serde::json::{json, Value, Json};
 use serde::Deserialize;
 use crate::errors::{Errors, FieldValidator};
@@ -20,11 +20,11 @@ pub struct NewSolution {
 #[derive(Deserialize, Validate)]
 pub struct NewSolutionData {
     #[validate(length(min = 1))]
-    title: Option<String>,
+    title: String,
     #[validate(length(min = 0))]
     description: Option<String>,
     #[validate(length(min = 1))]
-    code: Option<String>,
+    code: String,
 }
 
 
@@ -33,21 +33,19 @@ pub async fn post_solution(
     new_solution: Json<NewSolution>,
     db: Db,
 ) -> Result<Value, Errors> {
+
     let new_solution = new_solution.into_inner().solution;
-
     let mut extractor = FieldValidator::validate(&new_solution);
-    let title = extractor.extract("title", new_solution.title);
-    let description = extractor.extract("description", new_solution.description);
-    let code = extractor.extract("code", new_solution.code);
+    let title = extractor.extract("title", Some(new_solution.title));
+    let code = extractor.extract("code", Some(new_solution.code));
     extractor.check()?;
-
 
     let solution = db
         .run(move |conn| {
             database::solutions::create(
                 conn,
                 &title,
-                Some(&description),
+                new_solution.description.as_deref(),
                 &code,
             )
         })
